25654d14d3cb65416d05f921fa316d9fa7024896
AlessandroDalpiaz <alessandro.dalpiaz@gmail.com>
2022-04-07 14:27:30 +0200

    add fileConfig

--- database.js
+++ database.js
@@ -2,13 +2,16 @@ var util= require('util');
 //var encoder = new util.TextEncoder('utf-8');
 var cor_st= require('core-js/stable');
 var MongoClient = require('mongodb').MongoClient;
+var config = require('./local_config.js');
 
 //riga di esempio
 var row1= require('./quiz1.json');
 
 
 // Connection URL
-var url="mongodb://localhost:27017/db_quiz";
+//var url="mongodb://localhost:27017/db_quiz";
+var url="mongodb://"+config.connection.host+":"+config.port+"/"+config.connection.database;
+
 
 console.log("mi connetto a "+ url);
 
@@ -17,7 +20,7 @@ MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   console.log("connesso");
 
-  var dbo = db.db("db_quiz");
+  var dbo = db.db(config.connection.database);
   dbo.collection("quiz").insertOne(row1, function(err, res) {
     if (err) throw err;
 
---
+++ local_config.js
@@ -0,0 +1,14 @@
+var config =
+{
+    //ws_version: 'Version: 1.0 - 30/03/2022',
+    port: 27017,
+    connection:
+    {
+        host: 'localhost',
+        user: '',
+        password: '',
+        database: 'db_quiz'
+    }
+};
+
+module.exports = config;
